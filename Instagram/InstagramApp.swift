9053638d3d98be8246976e04643d4d73adefa679
Simon McNeil <simonmcneil062@gmail.com>
2022-12-19 00:28:02 -0500

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Simon Mcneil on 2022-12-19.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
